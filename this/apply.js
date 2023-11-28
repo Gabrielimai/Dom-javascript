eef31a185d1e6c23ffb1ccc1cc8da939890aca78
Gabriel Imai <imaig80@gmail.com>
2023-11-28 15:50:02 -0300

    complemento dos estudos dos metodos call e apply

--- this/apply.js
+++ this/apply.js
@@ -1,3 +1,21 @@
+// O método apply() chama uma função com um dado valor "this" e argumentos providos como um array (ou objeto parecido com um array)
+
+const obj1 = {exemplo:"Exemplo1", mostraThis:function mostraCallExemplo( a, b, c){
+  console.log(this)
+}}
+
+obj1.mostraThis()
+
+
+const obj2 = {exemplo2:"Exemplo2"}
+
+//Executa a função mostraThis e chama (aponta) o this do obj2
+obj1.mostraThis.apply(obj2, [1, 3, 6])
+
+
+//################################################################################################
+
+
 "user strict";
 
  this.name = "Camilla";
--- this/call.js
+++ this/call.js
@@ -2,7 +2,23 @@
 // Call do inglês chamar
 // Retorna o this no contexto que eu enviar
 
-// "use strict";
+// NOTA: call() e apply()  tem uma síntaxe indêntica. A diferença é que call() aceita uma lista de argumentos, enquanto apply() aceita um array de argumentos.
+
+//O método Call() invoca uma função com dado valor "this", e argumentos passados individualmente.
+
+const obj1 = {exemplo:"Exemplo1", mostraThis:function mostraCallExemplo(){
+  console.log(this)
+}}
+
+obj1.mostraThis()
+
+
+const obj2 = {exemplo2:"Exemplo2"}
+
+//Executa a função mostraThis e chama (aponta) o this do obj2
+obj1.mostraThis.call(obj2)
+
+//################################################################################################################
 
 this.name = "Camilla";
 
