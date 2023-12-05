e03af7ad3b23b3b341fbf309193165561cd69292
Gabriel Imai <imaig80@gmail.com>
2023-12-05 14:58:53 -0300

    organização dos arquivos

---
+++ ES6/arrow function.js
@@ -0,0 +1,33 @@
+// 1. Converta as funções abaixo para Arrow Functions.
+// ###########################################################
+function somar(a, b) {
+    return a + b;
+  }
+  
+  const somarArrow = (a, b) => a + b;
+  // ############################################################
+  function verificarNegativo(numero) {
+    return numero < 0;
+  }
+  
+  const verificarNegativoArrow = numero => numero < 0
+  // ############################################################
+  function criarUsuario(nome, idade) {
+    return {
+      nome,
+      idade,
+    };
+  }
+  
+  const criarUsuarioArrow = (name, idade) => ({
+      name,
+      idade, 
+  })
+  // ############################################################
+  setTimeout(function () {
+    console.log("Hello world!");
+  }, 1000);
+  
+  setTimeout(() => {
+      console.log('Hello world!');
+  }, 1000);
