6293b817b86ffc40fc2e105090e3f99d8a5b6b56
Gabriel Imai <imaig80@gmail.com>
2023-12-07 13:35:47 -0300

    Exercicios de filter

--- ES6/Filter.js
+++ ES6/Filter.js
@@ -35,3 +35,39 @@ const usuarios = [
 
 //   ######################################################### Exercicios ########################################################
 
+// 1. Crie uma lista que contenha apenas os valores que são maiores que 5 da lista "numeros".
+
+const numeros = [3, 6, 8, 2];
+
+const numerosMaioresQue5 = numeros.filter((numero) => {
+  return numero > 5;
+});
+
+console.log(numerosMaioresQue5);
+
+
+// 2. Crie uma lista que contenha apenas os valores pares da lista "numeros2".
+
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const numerosPares = numeros2.filter((par) => {
+  return par % 2 === 0;
+});
+
+console.log(numerosPares);
+// 3. Crie uma lista que contenha apenas as pessoas que têm idade o suficiente pra fazer parte da Matrix (a idade mínima é 18 anos), e logue-a no console.
+
+const pessoas = [
+    { nome: "Angelina Jolie", idade: 80 },
+    { nome: "Eric Jones", idade: 2 },
+    { nome: "Paris Hilton", idade: 5 },
+    { nome: "Kayne West", idade: 16 },
+    { nome: "Bob Ziroll", idade: 100 },
+  ];
+  
+  const maiorDeIdade = pessoas.filter((idade) => {
+    return idade.idade >= 18;
+  });
+  
+  console.log(maiorDeIdade)
+  
